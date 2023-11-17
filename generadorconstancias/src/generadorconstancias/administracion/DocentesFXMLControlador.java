e9abc7cf7cbe1a909208a306f3660b09ce6326a4
Johan <johnoficial004@gmail.com>
2023-11-17 10:46:31 -0600

    avanceViernes
    conexionConsultaSolicitudes

--- generadorconstancias/src/generadorconstancias/administracion/DocentesFXMLControlador.java
+++ generadorconstancias/src/generadorconstancias/administracion/DocentesFXMLControlador.java
@@ -5,11 +5,23 @@
 package generadorconstancias.administracion;
 
 import Modelo.POJO.PersonalAdministrativo;
+import Utilidades.Utilidades;
+import generadorconstancias.InicioSesionFXMLControlador;
+import generadorconstancias.administracion.constancia.ConsultarConstanciasSolicitadasFXMLControlador;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -49,6 +61,28 @@ public class DocentesFXMLControlador implements Initializable {
 
     @FXML
     private void consultarSolicitudes(ActionEvent event) {
+        if(!tvDocentes.getSelectionModel().isEmpty()){
+            try {
+                    int seleccionado = tvDocentes.getSelectionModel().getSelectedItem().getIdDocente();
+                    System.out.println(seleccionado);
+                    FXMLLoader loaderVentanaConsultarConstancias = new FXMLLoader(getClass().getResource("administracion/ConsultarConstanciasSolicitadasFXML.fxml"));
+                    Parent ventanaConsultarConstancias = loaderVentanaConsultarConstancias.load();
+
+                    Scene escenarioConstancias = new Scene(ventanaConsultarConstancias);
+                    Stage stageConstancias = new Stage();
+                    stageConstancias.setScene(escenarioConstancias);
+                    stageConstancias.initModality(Modality.APPLICATION_MODAL);
+
+                    ConsultarConstanciasSolicitadasFXMLControlador controlador = (ConsultarConstanciasSolicitadasFXMLControlador) loaderVentanaConsultarConstancias.getController();
+                    controlador.inicializarConstancias(seleccionado);
+
+                    stageConstancias.showAndWait();
+                } catch (IOException ex) {
+                    Logger.getLogger(InicioSesionFXMLControlador.class.getName()).log(Level.SEVERE, null, ex);
+                }
+        }else{
+            Utilidades.mostrarAlertaSimple("Selecciona un docente", "Se debe seleccionar un docente", Alert.AlertType.WARNING);
+        }
     }
 
     @FXML
